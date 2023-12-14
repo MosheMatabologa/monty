d8688d62021961073298f81b25d14a17c2ef31cf
MosheMatabologa <gideonmatabologa@gmail.com>
2023-12-14 03:28:26 -0800

    lots of work done

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - I aim to print the top only
+ * @head: stack head
+ * @counter: line_number(s)
+ * Return: no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
